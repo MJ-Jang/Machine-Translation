0d9d8595e38d86860f4ea8ad726325e07d619836
Myeongjun Jang <32857557+MJ-Jang@users.noreply.github.com>
2018-02-05 14:53:35 +0900

    Update Attention_bidirection.py

--- code/Attention_bidirection.py
+++ code/Attention_bidirection.py
@@ -1,10 +1,11 @@
 ####################################################
-# RNN-Variational Autoencoder model
+# Attention-Bidirectional translation model
 #  - Author: Myeongjun Jang
 #  - email: xkxpa@korea.ac.kr
 #  - git: https://github.com/MJ-Jang
 #  - version: Tensorflow ver 1.2.1
 ####################################################
+
 
 ################################################## 1. Import modules ###################################################
 import gensim
